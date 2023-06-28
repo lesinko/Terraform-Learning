d5d7519c83d3cf5e5d06f7d31c7ed1a44e6571a6
lesinko <lesinko2@gmail.com>
2023-06-28 13:15:02 +0300

    modified variables

--- Terraform-modules/aws-vpc-module/vpc-module.tf
+++ Terraform-modules/aws-vpc-module/vpc-module.tf
@@ -1,21 +1,21 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  version = "5.0.0" #lock to the specific version
 
   # vpc basic details
   name = "var.vpc_name"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = var.vpc_availability_zones
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
   #database subnets
-  database_subnets                   = ["10.0.150.0/24", "10.0.151.0/24"]
-  create_database_subnet_group       = true
-  create_database_subnet_route_table = true
+  database_subnets                   = var.vpc_database_subnets
+  create_database_subnet_group       = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   #NAT gateways
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
   #dns parameters
   enable_dns_hostnames = true
   enable_dns_support   = true
--- Terraform-modules/aws-vpc-module/vpc-variables.tf
+++ Terraform-modules/aws-vpc-module/vpc-variables.tf
@@ -10,3 +10,57 @@ variable "vpc_name" {
   type = string
   default = "dev-vpc"
 }
+
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_availability_zones" {
+  description = "vpc availability zones"
+  type = list(string)
+  default = [ "us-east-1a", "us-east-1b" ]
+}
+
+variable "vpc_public_subnets" {
+  description = "vpc public subnets"
+  type = list(string)
+  default = [ "10.0.101.0/24", "10.0.102.0/24" ]
+}
+
+variable "vpc_private_subnets" {
+  description = "vpc private subnets"
+  type = list(string)
+  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+}
+
+variable "vpc_database_subnets" {
+  description = "vpc database subnets"
+  type = list(string)
+  default = ["10.0.150.0/24", "10.0.151.0/24"]
+}
+
+variable "vpc_create_database_subnet_group" {
+  description = "vpc Create Database Subnet Group"
+  type = bool
+  default = true 
+}
+
+variable "vpc_create_database_subnet_route_table" {
+  description = "vpc Create Database Subnet Route Table"
+  type = bool
+  default = true   
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
+  type = bool
+  default = true  
+}
+
+variable "vpc_single_nat_gateway" {
+  description = "Enable only single NAT Gateway in one Availability Zone"
+  type = bool
+  default = true
+}
